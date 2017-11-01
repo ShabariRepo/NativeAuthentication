d0dca8d4e248034c83bd7c88158cfda18f9392ad
shareDemCodez <shabari.cs@gmail.com>
2017-11-01 18:09:58 -0400

    Copying reusable components - copy from albums - general folder structure

--- src/App.js
+++ src/App.js
@@ -11,39 +11,28 @@ import {
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Header } from './components/common';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
+        <Header headerText="Authentication" />
         <Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        </Text>        
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+// if this errors out remove the default
+// from the export statement in above files
+// i.e. export { Button };
